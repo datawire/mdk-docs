11cea64afc062cb21c48794cac79b26dc12e0ba5
Itamar Turner-Trauring <itamar@datawire.io>
2016-07-01 13:15:28 -0400

    Use Datawire theme for docs.

--- main/source/conf.py
+++ main/source/conf.py
@@ -122,7 +122,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx-datawire-theme'
+html_theme_path = ['../..']
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -336,4 +337,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
+intersphinx_mapping = {}  # {'https://docs.python.org/': None}
