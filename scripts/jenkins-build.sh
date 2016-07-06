910c82e7e9b23210bf051f0feed32d551b18121f
Itamar Turner-Trauring <itamar@datawire.io>
2016-07-06 11:35:43 -0400

    Use develop version of Quark.

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -30,7 +30,7 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | \
 nvm install 4.2.2 && nvm alias default 4.2.2
 
 # Do local setup
-make QUARKINSTALLARGS="-qqq -t $(pwd)/.autobuild-quark" setup
+make QUARKINSTALLARGS="-qqq -t $(pwd)/.autobuild-quark" QUARKBRANCH="develop" setup
 . $(pwd)/.autobuild-quark/config.sh
 
 export GIT_DEPLOY_DIR=dist
